3548841fdb5c0b2a308869f650fc1108cde919b8
kseta <keigo.seta@gmail.com>
2012-07-22 16:21:38 +0900

    空行を削除など

--- vending_machine/lib/drink_stock.rb
+++ vending_machine/lib/drink_stock.rb
@@ -1,13 +1,12 @@
 # -*- coding:UTF-8 -*-
 
 class DrinkStock
-   attr_accessor :stockhash
- 
-   def initialize
-	@stockhash =  { 
-		coke:{price:120, amount:5, name:"コーラ"}
-	}
-   end   
+  attr_accessor :stockhash
 
- 
+  def initialize
+    @stockhash =  {
+      coke:{price:120, amount:5, name:"コーラ"}
+    }
+  end
+
 end
