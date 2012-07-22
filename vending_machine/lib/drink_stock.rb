1baad4070ff77a57505c7e5e1a3d5b730fad3441
Jinhou Oh <jinhoou@gmail.com>
2012-07-22 16:21:27 +0900

    add drink_stock

---
+++ vending_machine/lib/drink_stock.rb
@@ -0,0 +1,13 @@
+# -*- coding:UTF-8 -*-
+
+class DrinkStock
+   attr_accessor :stockhash
+ 
+   def initialize
+	@stockhash =  { 
+		coke:{price:120, amount:5, name:"コーラ"}
+	}
+   end   
+
+ 
+end
---
+++ vending_machine/spec/drink_stock_spec.rb
@@ -0,0 +1,17 @@
+# -*- coding:UTF-8 -*-
+require 'drink_stock'
+
+describe DrinkStock do
+    before :each do
+        @drinkstock = DrinkStock.new
+    end
+
+    context "初期在庫の確認" do
+        it "初期在庫表示" do
+            @drinkstock.stockhash[:coke][:name].should == "コーラ"
+            @drinkstock.stockhash[:coke][:price].should == 120
+            @drinkstock.stockhash[:coke][:amount].should == 5 
+        end
+    end
+
+end
